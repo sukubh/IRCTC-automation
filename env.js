81549a6d447871fcd92a2a553440daeede1f064a
sukubh <159564484+sukubh@users.noreply.github.com>
2024-02-18 10:38:47 +0530

    version 2.0

---
+++ env.js
@@ -0,0 +1,7 @@
+
+let details = {
+    userName: 'bhandary_9',
+    Password: 'Sk@8056697281'
+};
+
+module.exports =  details ;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+let details = require('./env');
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -6,25 +7,71 @@ const puppeteer = require('puppeteer');
         defaultViewport: null,
         args: [
             `--window-size=${2080},${800}`,
-            `--user-data-dir=C:\Users\sushi\AppData\Local\Google\Chrome\User Data` // replace with the actual path to your data directory
         ]
-        // executablePath: 'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave', // replace with the actual path to your Brave browser executable
     });
     const page = await browser.newPage();
     await page.goto('https://www.irctc.co.in/nget/train-search');
     
-    // await page.waitForSelector('a.search_btn.loginText.ng-star-inserted');
-    // await page.click('a.search_btn.loginText.ng-star-inserted');
-    // await page.waitForSelector('input[placeholder="User Name"]');
-    // await page.type('input[placeholder="User Name"]', 'bhandary_9');
-    // await page.waitForSelector('input[placeholder="Password"]');
-    // await page.type('input[placeholder="Password"]', 'Sk@8056697281');
+    try {
+        
+        
+        // insert train detail 
+        await searchTrain(page)
 
-    // // Date
-    // await page.evaluate(() => document.querySelector('input.ng-tns-c58-10.ui-inputtext.ui-widget.ui-state-default.ui-corner-all.ng-star-inserted').value = '');
-    // const inputValue = await page.$eval('input.ng-tns-c58-10.ui-inputtext.ui-widget.ui-state-default.ui-corner-all.ng-star-inserted', el => el.value);
-    // await page.type('input.ng-tns-c58-10.ui-inputtext.ui-widget.ui-state-default.ui-corner-all.ng-star-inserted', '01/03/2024');
+        // seting the Date
+        await setdate(page)
 
+        // login
+        await login(page)
+    } catch (error) {
+        console.log("err-->", error);
+    }
+
+})();
+
+const setdate = async(page) => {
+    // Date
+    await page.waitForSelector('input[placeholder="DD/MM/YYYY *"]')
+    await page.evaluate(() => document.querySelector('input[placeholder="DD/MM/YYYY *"]').value = '');
+    const inputValue = await page.$eval('input[placeholder="DD/MM/YYYY *"]', el => el.value);
+    await page.type('input[placeholder="DD/MM/YYYY *"]', '18/02/2024');
+    // await page.keyboard.press('Enter');
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await page.click('button[class="hidden-xs search_btn btn"]')
+}
+
+const login = async(page) => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await page.waitForSelector('a.search_btn.loginText.ng-star-inserted');
+    await page.click('a.search_btn.loginText.ng-star-inserted');
+    await page.waitForSelector('input[placeholder="User Name"]');
+    // await page.click('input[placeholder="User Name"]');
+    await page.type('input[placeholder="User Name"]', details.userName);
+    await new Promise(resolve => setTimeout(resolve, 500));
+    // await page.click('input[placeholder="Password"]');
+    await page.waitForSelector('input[placeholder="Password"]');
+    await page.type('input[placeholder="Password"]', details.Password);
+    await new Promise(resolve => setTimeout(resolve, 500));
+    // await page.waitForSelector('button[style="padding: 10px 14px;"]')
+    await page.waitForSelector('input[id="captcha"]');
+  
+    // You can prompt the user here to fill in the CAPTCHA manually
+    console.log('Please fill in the CAPTCHA.');
+
+    // Wait for user to fill in the CAPTCHA manually
+    await page.evaluate(() => {
+        const button = document.querySelector('button[style="padding: 10px 14px;"]');
+        button.addEventListener('click', () => {
+            button.clicked = true;
+        });
+    });
+
+    console.log('CAPTCHA is filled');
+}
+
+const searchTrain = async(page) => {
+    // To check all the compounent are mounted
+    await new Promise(resolve => setTimeout(resolve, 1000));
     // To check all the compounent are mounted
     await page.waitForSelector('input.ng-tns-c57-8.ui-inputtext.ui-widget.ui-state-default.ui-corner-all.ui-autocomplete-input.ng-star-inserted');
     
@@ -33,9 +80,9 @@ const puppeteer = require('puppeteer');
     await page.waitForSelector('li[aria-label="Sleeper (SL)"]')
     await page.click('li[aria-label="Sleeper (SL)"]')
     // //Genaral
-    // await page.click('span.ui-dropdown-trigger-icon.ui-clickable.ng-tns-c65-12.pi.pi-chevron-down')
-    // await page.waitForSelector('li[aria-label="TATKAL"]')
-    // await page.click('li[aria-label="TATKAL"]')
+    await page.click('span.ui-dropdown-trigger-icon.ui-clickable.ng-tns-c65-12.pi.pi-chevron-down')
+    await page.waitForSelector('li[aria-label="TATKAL"]')
+    await page.click('li[aria-label="TATKAL"]')
 
     //From
     await page.type('input.ng-tns-c57-8.ui-inputtext.ui-widget.ui-state-default.ui-corner-all.ui-autocomplete-input.ng-star-inserted', 'HOSUR - HSRA');
@@ -44,12 +91,4 @@ const puppeteer = require('puppeteer');
     await page.type('input.ng-tns-c57-9.ui-inputtext.ui-widget.ui-state-default.ui-corner-all.ui-autocomplete-input.ng-star-inserted', 'COIMBATORE JN - CBE (COIMBATORE)');
     // await page.waitForTimeout(2000);
     await page.keyboard.press('Enter');
-
-    // Date
-    await page.waitForSelector('input[placeholder="DD/MM/YYYY *"]')
-    await page.evaluate(() => document.querySelector('input[placeholder="DD/MM/YYYY *"]').value = '');
-    const inputValue = await page.$eval('input[placeholder="DD/MM/YYYY *"]', el => el.value);
-    await page.type('input[placeholder="DD/MM/YYYY *"]', '01/03/2024');
-    await page.click('button.hidden-xs.search_btn.btn')
-
-})();
+}
